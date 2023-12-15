a4e12bf766ab12ac5a40a76a97f06a78b7709ab7
ankh <ankh04@icloud.com>
2023-12-15 15:09:46 +0800

    mvvm complete. Style remains handling

---
+++ app/src/main/java/host/ankh/beatbox/Sound.kt
@@ -0,0 +1,6 @@
+package host.ankh.beatbox
+
+private const val WAV = ".wav"
+class Sound(val assetPath: String, var soundId: Int? = null) {
+    val name = assetPath.split("/").last().removeSuffix(WAV)
+}
